6b18fc5a92e46a1a711d19af5f0c374df2c9abd8
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2022-11-06 20:49:11 +0100

    Add files via upload

---
+++ Zad 4/Zad 4/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Zad_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.WriteLine("Podaj a");
+
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj b");
+
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj c");
+
+            c = int.Parse(Console.ReadLine());
+
+            if (a > b && b > c)
+
+                Console.WriteLine(" Najwieksze jest " + a);
+
+            else if (b > a && a > c)
+                Console.WriteLine("Najwieksze jest " + b);
+
+            else
+                Console.WriteLine("Najwieksze jest " + c);
+
+            Console.ReadKey();
+        
+        }
+    }
+}
